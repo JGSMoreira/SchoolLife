f1f4f2499143eab7b0a0a6165031b76cfa6b7e41
96486805234 <96486805234@JIPA-LABIN02-05.ifro.local>
2017-10-30 12:03:56 -0400

    aaaa

--- School Life/src/Professor/CadastroProfessor.java
+++ School Life/src/Professor/CadastroProfessor.java
@@ -1,7 +1,11 @@
 package Professor;
 
+import java.awt.Color;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,10 +13,10 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JTextField;
 
-public class CadastroProfessor extends JFrame implements ActionListener{
+public class CadastroProfessor extends JFrame implements ActionListener, MouseListener{
 	
 	private JLabel lblNome = new JLabel("Nome:"),
-				   lblCodigo = new JLabel("Código");
+				   lblCodigo = new JLabel("Código:");
 	private JTextField txtNome = new JTextField(),
 					   txtCódigo = new JTextField();
 	private JButton btnSalvar = new JButton("Salvar"),
@@ -21,20 +25,39 @@ public class CadastroProfessor extends JFrame implements ActionListener{
 	
 	
 	public CadastroProfessor () {
-		setBounds(100,100,500,500);
+		
+		//Definições da janela
+		setLayout(null);
+		setBounds(100,100,300,400);
 		setTitle("Cadastro de Professor");
 		setVisible(true);
 		setResizable(false);
 		
 		btnFechar.addActionListener(this);
 		btnSalvar.addActionListener(this);
-		
 
-		lblNome.setBounds(10, 10, 10, 10);
+		lblNome.setBounds(50, 50, 50, 20);
 		add(lblNome);
-		lblCodigo.setBounds(30, 10, 20, 10);
+		
+		lblCodigo.setBounds(50, 100, 50, 20);
 		add(lblCodigo);
-	}
+		
+		txtNome.setBounds(50, 60, 100, 20);
+		
+		btnFechar.setBounds(30, 300,  80, 30);
+		add(btnFechar);
+		btnSalvar.setBounds(180, 300, 80, 30);
+		add(btnSalvar);
+		
+		btnSalvar.setBackground(new Color(240, 255, 240));
+		btnSalvar.setOpaque(true);
+		
+		btnSalvar.addMouseListener(this);
+	
+
+	
+
+		}
 	
 	public void actionPerformed(ActionEvent e) {
 		
@@ -42,6 +65,35 @@ public class CadastroProfessor extends JFrame implements ActionListener{
 	
 	public static void main(String[] args) {
 		CadastroProfessor c = new CadastroProfessor();
+	}
+
+
+	public void mouseClicked(MouseEvent e) {
+		btnSalvar.setBackground(Color.WHITE);
+		
+	}
+
+
+	public void mouseEntered(MouseEvent e) {
+		btnSalvar.setBackground(Color.YELLOW);
+		
+	}
+
+
+	public void mouseExited(MouseEvent e) {
+		btnSalvar.setBackground(Color.PINK  );
+		
+	}
+
+
+	public void mousePressed(MouseEvent e) {
+		btnSalvar.setBackground(Color.GREEN);
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		btnSalvar.setBackground(Color.RED);
+		
 	}
 
 	
