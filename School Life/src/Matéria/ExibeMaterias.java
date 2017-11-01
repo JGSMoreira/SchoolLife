68c372d3ebbefc0810181f50feb657ef235069d6
MineSlim <mine.slimrt@gmail.com>
2017-10-31 22:08:28 -0400

    Nem eu sei mais.

---
+++ School Life/src/Matéria/ExibeMaterias.java
@@ -0,0 +1,15 @@
+package Matéria;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class ExibeMaterias extends JFrame implements ActionListener {
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
